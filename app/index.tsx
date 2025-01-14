711ce3f5537825f15e975fa45f5971578887fc5f
picupiee <picupiee.id@gmail.com>
2025-01-14 22:01:02 +0700

    first edit

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,12 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="h-full w-full">
+      <View className="flex items-center justify-center bg-blue-500 p-4 mt-4">
+        <Text className="font-medium text-xl text-white">
+          Edit app/index.tsx to edit this screen.
+        </Text>
+      </View>
     </View>
   );
 }
