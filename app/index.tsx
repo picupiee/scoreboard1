4418ab92d47f1b13d9e9c4e602ecbd236dd4774c
picupiee <picupiee.id@gmail.com>
2025-01-14 22:55:39 +0700

    updating menu's button

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,37 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="h-full w-full">
-      <View className="flex items-center justify-center mt-4">
-        <Text className="font-medium text-xl text-white py-2 px-4 rounded-lg bg-blue-500">
+      <View className="flex items-center justify-center mt-8">
+        <Text className="font-medium text-3xl text-white py-2 px-4 rounded-lg bg-blue-500">
           Scoreboard App
         </Text>
       </View>
-      <View className="flex flex-col items-center">
-        <Link href="/game-setup" className="bg-blue-600 mt-4 p-2 rounded-lg">
-          <Text className="text-xl text-white font-medium">Menu 1</Text>
+      <View className="flex flex-col items-center mt-5">
+        <Link href="/game-setup" className="my-2">
+          <TouchableOpacity className="bg-blue-600 rounded-lg">
+            <Text className="text-xl text-white font-medium p-2">Menu 1</Text>
+          </TouchableOpacity>
         </Link>
 
-        <Link href="/game-history" className="bg-blue-600 mt-4 p-2 rounded-lg">
-          <Text className="text-xl text-white font-medium">Menu 2</Text>
+        <Link href="/game-history" className="my-2">
+          <TouchableOpacity className="bg-lime-600 rounded-lg">
+            <Text className="text-xl text-white font-medium p-2">Menu 2</Text>
+          </TouchableOpacity>
         </Link>
 
-        <Link href="/game-settings" className="bg-blue-600 mt-4 p-2 rounded-lg">
-          <Text className="text-xl text-white font-medium">Menu 3</Text>
+        <Link href="/game-settings" className="my-2">
+          <TouchableOpacity className="bg-red-500 rounded-lg">
+            <Text className="text-xl text-white font-medium p-2">Menu 3</Text>
+          </TouchableOpacity>
         </Link>
 
-        <Link href="/game-about" className="bg-blue-600 mt-4 p-2 rounded-lg">
-          <Text className="text-xl text-white font-medium">Menu 4</Text>
+        <Link href="/game-about" className="my-2">
+          <TouchableOpacity className="bg-slate-400 rounded-lg">
+            <Text className="text-xl text-white font-medium p-2">Menu 4</Text>
+          </TouchableOpacity>
         </Link>
       </View>
     </View>
