267d1058abc3c4d49540cd17a275c7cd27612931
picupiee <picupiee.id@gmail.com>
2025-01-16 19:41:19 +0700

    updating RoundHistoryTable

--- app/scoreboard.tsx
+++ app/scoreboard.tsx
@@ -1,11 +1,18 @@
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
-import { Alert, Modal, Platform, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Modal,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import RoundHistoryTable from "@/components/RoundHistoryTable";
 
-
 export default function Scoreboard() {
-  const router = useRouter()
+  const router = useRouter();
   const params = useLocalSearchParams();
   const teamNames = JSON.parse(params.teamNames);
   const numRounds = parseInt(params.numRounds, 10);
@@ -15,9 +22,12 @@ export default function Scoreboard() {
   const [roundScores, setRoundScores] = useState(Array(4).fill(0));
 
   const [roundHistory, setRoundHistory] = useState<
-    { round: number; scores: [number, number, number, number]; submittedScores: [number, number, number, number] }[]
+    {
+      round: number;
+      scores: [number, number, number, number];
+      submittedScores: [number, number, number, number];
+    }[]
   >([]);
-
 
   const handleEndRounds = () => {
     setModalVisible(true);
@@ -28,6 +38,17 @@ export default function Scoreboard() {
       (score, index) => score + roundScores[index]
     );
     setScores(updatedScores);
+
+    setRoundHistory([
+      ...roundHistory,
+      {
+        round: roundHistory.length + 1,
+        scores: [...updatedScores] as [number, number, number, number],
+        submittedScores: [...roundScores] as [number, number, number, number],
+      },
+    ]);
+
+    console.log(roundHistory);
 
     // Reset the round scores
     setRoundScores(Array(4).fill(0));
@@ -43,33 +64,33 @@ export default function Scoreboard() {
   };
 
   const handleFinishGame = () => {
-  if (Platform.OS === "web") {
-    if (window.confirm("Are you sure you want to finish the game?")) {
-      router.replace("/");
-    } 
-  } else {
-    Alert.alert(
-      "Finishing Game",
-      "Are you sure you want to finish the game ?",
-      [
-        {
-          text: "No",
-          style: "cancel",
-        },
-        {
-          text: "Yes",
-          onPress: () => {
-            router.replace("/");
+    if (Platform.OS === "web") {
+      if (window.confirm("Are you sure you want to finish the game?")) {
+        router.replace("/");
+      }
+    } else {
+      Alert.alert(
+        "Finishing Game",
+        "Are you sure you want to finish the game ?",
+        [
+          {
+            text: "No",
+            style: "cancel",
           },
-        },
-      ]
-    )
-  }
+          {
+            text: "Yes",
+            onPress: () => {
+              router.replace("/");
+            },
+          },
+        ]
+      );
+    }
   };
 
   return (
     <View className="h-full">
-      <View className="flex flex-row flex-wrap gap-8 items-center justify-center m-4 p-10">
+      <View className="flex flex-row flex-wrap sm:gap-8 gap-4 items-center justify-center m-2 mt-10 p-5 sm:m-4 sm:p-10">
         {teamNames.map((name, index) => (
           <View
             key={index}
@@ -82,12 +103,14 @@ export default function Scoreboard() {
           </View>
         ))}
       </View>
-      <View className="mt-2 p-4 mx-10 flex-row items-center justify-around">
+      <View className="mt-2 p-4 sm:mx-10 mx-5 flex-row items-center justify-around">
         <TouchableOpacity
           onPress={handleEndRounds}
           className="bg-black w-40 h-24 rounded flex justify-center"
         >
-          <Text className="text-white text-lg font-semibold text-center">End Round</Text>
+          <Text className="text-white text-lg font-semibold text-center">
+            End Round
+          </Text>
         </TouchableOpacity>
 
         {/* Modal view for submitting score */}
@@ -96,14 +119,14 @@ export default function Scoreboard() {
             <Text className="mb-4 mt-10 text-center text-2xl font-semibold underline">
               Enter Round Scores
             </Text>
-            <View className="flex-row items-center justify-center flex-wrap gap-8 p-10">
+            <View className="flex-row items-center justify-center flex-wrap gap-5 sm:gap-8 p-5 sm:p-10">
               {teamNames.map((name, index) => (
                 <View
                   key={index}
                   className="w-44 border border-gray-400 p-4 flex-col items-center justify-center"
                 >
                   <Text className="text-2xl font-semibold">{name}</Text>
-                  <View className="w-1/2 h-10 border mt-2">
+                  <View className="w-20 h-10 border mt-2">
                     <TextInput
                       className="text-2xl font-bold px-1 py-1 text-center placeholder:text-red-200"
                       placeholder="..."
@@ -112,35 +135,45 @@ export default function Scoreboard() {
                         handleRoundScoreChange(index, text)
                       }
                       maxLength={4}
+                      keyboardType="numeric"
                     />
                   </View>
                 </View>
               ))}
             </View>
-            <View className="flex flex-row items-center justify-center gap-4">
-              
-            <TouchableOpacity className="w-1/4" onPress={handleInputScore}>
-              <Text className="bg-blue-500 text-white px-4 py-2 rounded text-center text-lg">
-                Submit Score
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              className="w-1/4"
-              onPress={() => setModalVisible(false)}
-            >
-              <Text className="bg-red-500 text-white px-4 py-2 rounded text-center text-lg">Cancel</Text>
-            </TouchableOpacity>
+            <View className="flex flex-row items-center justify-center gap-4 mt-5">
+              <TouchableOpacity
+                className="w-1/3 sm:w-1/4"
+                onPress={handleInputScore}
+              >
+                <Text className="bg-blue-500 text-white px-4 py-2 rounded text-center text-lg">
+                  Submit Score
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                className="w-1/4"
+                onPress={() => setModalVisible(false)}
+              >
+                <Text className="bg-red-500 text-white px-4 py-2 rounded text-center text-lg">
+                  Cancel
+                </Text>
+              </TouchableOpacity>
             </View>
           </View>
         </Modal>
 
-        <TouchableOpacity onPress={handleFinishGame} className=" bg-red-400 w-40 h-24 rounded flext justify-center"> 
-          <Text className="text-xl font-semibold text-center text-white">Finish Game</Text>
+        <TouchableOpacity
+          onPress={handleFinishGame}
+          className=" bg-red-400 w-40 h-24 rounded flext justify-center"
+        >
+          <Text className="text-xl font-semibold text-center text-white">
+            Finish Game
+          </Text>
         </TouchableOpacity>
       </View>
 
-      <View className="p-4 mx-10">
-      <RoundHistoryTable roundHistory={roundHistory} teamNames={teamNames} />
+      <View className="p-4 mx-6 sm:mx-10">
+        <RoundHistoryTable roundHistory={roundHistory} teamNames={teamNames} />
       </View>
     </View>
   );
--- components/RoundHistoryTable.tsx
+++ components/RoundHistoryTable.tsx
@@ -15,19 +15,38 @@ const RoundHistoryTable = ({
 }: RoundHistoryTableProps) => {
   return (
     <View className="mt-4">
-      <Text className="font-bold mb-4">Round History</Text>
+      <Text className="font-bold mb-4 text-lg border-b border-spacing-5">
+        Round History
+      </Text>
+      <View className="flex flex-row items-center justify-around gap-5">
+        <Text>Round</Text>
+        {teamNames.map((name, index) => (
+          <View key={index} className="mx-2 my-2">
+            <Text key={index} className="mt-2">
+              {name}
+            </Text>
+          </View>
+        ))}
+      </View>
+
       {roundHistory.map((roundData) => (
         <View
           key={roundData.round}
-          className="flex flex-row justify-between border-b border-gray-200 pb-2"
+          className="flex flex-row justify-between items-center gap-5"
         >
-          <Text>Round {roundData.round}</Text>
+          <Text className="mr-2 mb-2 border p-2">#{roundData.round}</Text>
           {teamNames.map((name, index) => (
-            <Text key={index}>
-              {roundData.scores[index]} (
-              {roundData.submittedScores[index] >= 0 ? "+" : ""}
-              {roundData.submittedScores[index]})
-            </Text>
+            <View
+              key={index}
+              className="flex flex-col items-center justify-center mr-5"
+            >
+              <Text key={index}>{name}</Text>
+              <Text>{roundData.scores[index]}</Text>
+              <Text>
+                ({roundData.submittedScores[index] > 0 ? "+" : ""}
+                {roundData.submittedScores[index]})
+              </Text>
+            </View>
           ))}
         </View>
       ))}
